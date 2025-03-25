cc04ceccf072f15dd624c68132732769b2deac52
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2025-03-25 17:54:25 +0530

    tweak : removed error

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -166,7 +166,7 @@ export default function SignInPage() {
         </Form>
 
         <div className="text-center text-sm">
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <Link
             href="/sign-up"
             className="font-medium text-primary hover:underline"
